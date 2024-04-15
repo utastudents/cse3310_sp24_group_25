07ff35920a5f28bfb0da91f72e1a3f039a35450e
XSolis <xochith.solis@mavs.uta.edu>
2024-04-15 15:37:40 -0500

    added grid test

---
+++ src/test/java/uta/cse3310/WordGridTest.java
@@ -0,0 +1,24 @@
+package uta.cse3310;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WordGridTest
+    extends TestCase
+    {
+        public WordGridTest (String testName){
+            super(testName);
+        }
+
+        public static Test suite(){
+            return new TestSuite(WordGridTest.class);
+        }
+
+        public void WordGridTest(){
+            WordGrid w = new WordGrid(3);
+            WordSelection t = new WordSelection(0, 1, 0, 7);
+
+            assertTrue(w.CheckWord(t));
+        }
+    }
