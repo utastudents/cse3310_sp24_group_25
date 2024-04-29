17560aadfe0f18fa2f084e7386d839316296a0fe
axo4708 <158520185+axo4708@users.noreply.github.com>
2024-04-28 22:22:04 -0500

    Update WordSelectionTest.java
    Accidentally merged earlier change, reverted back to original

--- src/test/java/uta/cse3310/WordSelectionTest.java
+++ src/test/java/uta/cse3310/WordSelectionTest.java
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 package uta.cse3310;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import junit.framework.Assert;
-
-import static org.junit.Test;
-import static org.junit.Assert.*;
-
-public class WordSelectionTest 
-{
-=======
 //import org.junit.Test;
 //import static org.junit.Assert.assertEquals;
 
@@ -19,27 +7,22 @@ public class WordSelectionTest
 {
 
     //@Test
->>>>>>> 30044bfd5fb5d205f46a1fea360f87c68ba0b82e
     public void testCompareCoordinates() 
     {/*
         WordSelection ws = new WordSelection(1, 2, 3, 4, 1, 2, 3, 4);
         boolean result = ws.compareCoordinates();
-        assertTrue(result);
+        assertEquals(true, result);
 
         WordSelection ws2 = new WordSelection(1, 2, 3, 4, 3, 4, 1, 2);
         boolean result2 = ws2.compareCoordinates();
-        assertTrue(result2);
+        assertEquals(true, result2);
 
         WordSelection ws3 = new WordSelection(1, 2, 3, 4, 3, 4, 5, 6);
         boolean result3 = ws3.compareCoordinates();
-        assertTrue(result3);
+        assertEquals(false, result3);
 
         WordSelection ws4 = new WordSelection(1, 2, 3, 4, 5, 6, 1, 2);
         boolean result4 = ws4.compareCoordinates();
-<<<<<<< HEAD
-        assertTrue(result4);
-=======
         assertEquals(false, result4);*/
->>>>>>> 30044bfd5fb5d205f46a1fea360f87c68ba0b82e
     }
 }
